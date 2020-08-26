e43de123fc7a764b6468cba171394e15caecd63e
thisRandomName <49452494+thisRandomName@users.noreply.github.com>
2020-08-26 22:25:48 +0300

    Update form_validation.js

--- formValidation/form_validation.js
+++ formValidation/form_validation.js
@@ -7,7 +7,7 @@ window.addEventListener("load",function(){
     //which is a string (making the function treat the string as an array of chars) as the first parameter that call gets is the context.
     // the filter function returns a subset of the array for each element that returns true
     let validation=Array.prototype.filter.call(fields,
-        //for every form in forms :
+        //for every field in fields :
         function(field){
             //execute function(e) when the submit event occurs:
             field.addEventListener("submit",
